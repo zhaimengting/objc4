6f4f2aba10ee1fae6d3073c86be4cb13ff003494
zhaimengting <942170660@qq.com>
2020-12-14 19:54:35 +0800

    添加Objc-781编译源码

---
+++ objc4-781/MTFile/main.m
@@ -0,0 +1,24 @@
+//
+//  main.m
+//  objc
+//
+//  Created by mac on 2020/12/14.
+//
+
+#import <Foundation/Foundation.h>
+#import <Foundation/Foundation.h>
+//#import "MTStudent.h"
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        
+        
+//        // NSObject *objc1 = [NSObject alloc];
+//        MTStudent *objc2 = [MTStudent alloc];
+//        // LGPerson *objc3 = [LGPerson alloc];
+//
+//        NSLog(@"Hello, World!  %@",objc2);
+    }
+    return 0;
+}
